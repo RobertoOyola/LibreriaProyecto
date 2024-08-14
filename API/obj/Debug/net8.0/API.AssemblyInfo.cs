5b36d1ee60a0431e5d4b25e3a5c74bbb1d171e13
Roberto Oyola Vera <roberto.oyola@tecnologicoliceocristiano.edu.ec>
2024-08-13 22:37:32 -0500

    a

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f2fd10f6a3ef350deaf4723e035489db2a5acff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06d0f1efa8b3e6d2d84941b7d89ded0074b02fbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
